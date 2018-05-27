4cd549b388157c701249717be0fb42663269b973
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2018-05-27 17:28:45 +0200

    Update test

--- dao/rows.test.js
+++ dao/rows.test.js
@@ -1,5 +1,5 @@
 import unexpected from 'unexpected';
-import dao, { insert } from './rows';
+import dao, { insert, update } from './rows';
 
 const expect = unexpected.clone();
 
@@ -58,6 +58,66 @@ describe("rows dao", () => {
         return expect(insert(clientMock, 111, { _id: 77, cid: 777, title: "testtitle", colvalues: { 1: "v1" } }), 'to be rejected with', /test/);
     });
 
-    // TODO: update test
+    it("updates existing rows", () => {
+        const clientMock = { query: jest.fn() };
+        clientMock.query.mockImplementation((q, p) => Promise.resolve({
+            rows: (q.indexOf("n.newid::int") >= 0 ? [
+                { newid: 99 }
+            ] : q.indexOf("m.maxv::int") >= 0 ? [
+                { maxv: 2 }
+            ] : null), 
+            rowCount: (q.indexOf("update rec set archived=TRUE") >= 0 ? 1 : null)
+        }));
+        return update(clientMock, 111, { _id: 77, cid: 777, version: 2, title: "testtitlee", colvalues: { 1: "v11" } }).then(res => {
+            expect(clientMock.query.mock.calls.length, 'to be', 6);
+            expect(clientMock.query.mock.calls[0][0], 'to be', 'begin');
+            expect(clientMock.query.mock.calls[1], 'to satisfy', [/update rec set archived=TRUE.*/, [777, 2]]);
+            expect(clientMock.query.mock.calls[2][0], 'to satisfy', /select.*newid.*seq_rec_id.*/);
+            expect(clientMock.query.mock.calls[3], 'to satisfy', [/insert into rec .*/, [99, 111, 777, 3, "testtitlee"]]);
+            expect(clientMock.query.mock.calls[4], 'to satisfy', [/insert into reccol .*/, [99, 1, "v11"]]);
+            expect(clientMock.query.mock.calls[5][0], 'to be', 'commit');
+            expect(res, 'to satisfy', { ok: true, _id: 99, version: 3 });
+        }); // (catch is handled by the framework)
+    });
+
+    it("updates existing rows ignoring version", () => {
+        const clientMock = { query: jest.fn() };
+        clientMock.query.mockImplementation((q, p) => Promise.resolve({
+            rows: (q.indexOf("n.newid::int") >= 0 ? [
+                { newid: 99 }
+            ] : q.indexOf("m.maxv::int") >= 0 ? [
+                { maxv: 33 }
+            ] : null), 
+            rowCount: (q.indexOf("update rec set archived=TRUE") >= 0 ? 1 : null)
+        }));
+        return update(clientMock, 111, { _id: 77, cid: 777, version: 2, title: "testtitlee", colvalues: { 1: "v11" } }, true).then(res => {
+            expect(clientMock.query.mock.calls.length, 'to be', 7);
+            expect(clientMock.query.mock.calls[0][0], 'to be', 'begin');
+            expect(clientMock.query.mock.calls[1], 'to satisfy', [/select m.maxv::int.*/, [777]]);
+            expect(clientMock.query.mock.calls[2], 'to satisfy', [/update rec set archived=TRUE.*/, [777, 33]]);
+            expect(clientMock.query.mock.calls[3][0], 'to satisfy', /select.*newid.*seq_rec_id.*/);
+            expect(clientMock.query.mock.calls[4], 'to satisfy', [/insert into rec .*/, [99, 111, 777, 34, "testtitlee"]]);
+            expect(clientMock.query.mock.calls[5], 'to satisfy', [/insert into reccol .*/, [99, 1, "v11"]]);
+            expect(clientMock.query.mock.calls[6][0], 'to be', 'commit');
+            expect(res, 'to satisfy', { ok: true, _id: 99, version: 34 });
+        }); // (catch is handled by the framework)
+    });
+
+    it("updates existing rows detecting mismatching version", () => {
+        const clientMock = { query: jest.fn() };
+        clientMock.query.mockImplementation((q, p) => Promise.resolve({
+            rows: (q.indexOf("n.newid::int") >= 0 ? [
+                { newid: 99 }
+            ] : null), 
+            rowCount: (q.indexOf("update rec set archived=TRUE") >= 0 ? 0 : null)
+        }));
+        return expect(update(clientMock, 111, { _id: 77, cid: 777, version: 2, title: "testtitlee", colvalues: { 1: "v11" } }), 'to be rejected with', { extmsg: /.*no longer active.*/ });
+    });
+
+    it("updates existing rows handling error", () => {
+        const clientMock = { query: jest.fn() };
+        clientMock.query.mockImplementation((q, p) => (('begin' === q || 'rollback' === q) ? Promise.resolve() : Promise.reject("test")));
+        return expect(update(clientMock, 111, { _id: 77, cid: 777, version: 2, title: "testtitle", colvalues: { 1: "v1" } }), 'to be rejected with', /test/);
+    });
 
 });
